4be1ca6e7fe807db138c9bd16a8bfaa23fa1c52c
Ubaldo Sanchez <you@example.com>
2025-08-21 20:14:32 -0300

    arreglos

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/paginaPesonal/',   
-  server: {
-    port: 3001,              
-  },
   optimizeDeps: {
-    exclude: ['lucide-react'], 
+    exclude: ['lucide-react'],
   },
-})
+});
